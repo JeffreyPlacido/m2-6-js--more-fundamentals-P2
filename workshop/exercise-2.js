390230426060384b17b4adc39ed3ef43a11cb0b5
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-23 13:32:55 -0400

    E2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,14 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+
+function createGuestOrder(early, late) {
+    let guests = [...early, ...late];
+
+    guests.forEach((attendee, index) => {
+    console.log(`${index + 1}. ${attendee}`);
+    });
+}
+
+createGuestOrder(earlyBirds, lateComers);
