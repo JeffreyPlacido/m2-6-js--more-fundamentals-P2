17effcc0c7dcb09cb9776f1e3985e0df87a210c8
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-23 13:19:48 -0400

    Exercise 1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -1,6 +1,3 @@
-// Exercise 1
-// ----------
-
 let mostPopularFood = [
   'poke bowl',
   'chicken sliders',
@@ -12,3 +9,30 @@ let mostPopularFood = [
   'parmesan chicken',
   undefined,
 ];
+
+//step 1
+
+const fixList = (arr, first, last) => {
+
+  const fixedList = [...arr];
+
+  fixedList.pop();
+  fixedList.unshift(first);
+  fixedList.push(last);
+
+
+  return fixedList;
+
+};
+
+//step 2
+
+const printList = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log( (i + 1) + (arr[i]));
+  }
+};
+
+// call
+
+printList(fixList(mostPopularFood, 'bean burritos', 'buffalo-flavored cauliflower'));
